f85e5616f2b74ed67f927fb70f3b8f7c5c1cc89d
ah682 <96323387+ah682@users.noreply.github.com>
2024-01-08 03:49:47 +0000

    Implement Dyslexia Mode
    Remove Dropdown and Replaced with Button With Partially Functioning Dyslexia Function

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
-document.querySelector('.upload-button').addEventListener('click', function() {
-  document.getElementById('hiddenFileInput').click();
+document.querySelector(".upload-button").addEventListener("click", function () {
+  document.getElementById("hiddenFileInput").click();
 });
 
 function handleFileUpload(input) {
   if (input.files && input.files[0]) {
-      // Handle the file upload here, e.g., display the file name or upload it
-      console.log("File Uploaded: ", input.files[0].name);
-      // Additional actions upon file selection
+    // Handle the file upload here, e.g., display the file name or upload it
+    console.log("File Uploaded: ", input.files[0].name);
+    // Additional actions upon file selection
   }
+}
+
+function activateDyslexiaAssist() {
+  // Add code here to toggle dyslexia-friendly styles
+  document.body.classList.toggle("dyslexia-mode");
+
+  // You can add more changes such as changing font, colors etc.
 }
