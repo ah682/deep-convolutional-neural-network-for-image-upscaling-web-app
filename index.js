70bdefe739884d7e4141abcd1b718595e70c0a58
ah682 <96323387+ah682@users.noreply.github.com>
2024-01-08 04:46:54 +0000

    Implement partial colourblindess functions
    Implement partial colourblindess functions for various colourblindness types via dropdown button

--- index.js
+++ index.js
@@ -16,3 +16,17 @@ function activateDyslexiaAssist() {
 
   // You can add more changes such as changing font, colors etc.
 }
+
+function setColorBlindMode(mode) {
+  var bodyElement = document.body;
+  // Clear all previous colorblind classes
+  bodyElement.classList.remove(
+    "no-color-blindness",
+    "deuteranopia",
+    "protanopia",
+    "tritanopia",
+    "monochromatism"
+  );
+  // Add the selected mode
+  bodyElement.classList.add(mode);
+}
